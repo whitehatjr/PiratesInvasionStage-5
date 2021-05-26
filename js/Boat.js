18583df64d0f307e6d02c4fa83974df72974e15b
whitehatjr <whitehatjr@whitehatjr.com>
2021-05-26 14:21:13 +0530

    updated assets and canvas size

--- js/Boat.js
+++ js/Boat.js
@@ -23,8 +23,8 @@ class Boat {
   remove(index) {
     this.animation = brokenBoatAnimation;
     this.speed = 0.05;
-    this.width = 350;
-    this.height = 350;
+    this.width = 300;
+    this.height = 300;
     setTimeout(() => {
       Matter.World.remove(world, boats[index].body);
       boats.splice(index, 1);
